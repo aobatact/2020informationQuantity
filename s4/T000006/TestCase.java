ecc90b2483348b85d2ca63dfa31895e9b75a2bed
aobatact <aobatact144@gmail.com>
2020-12-14 17:46:25 +0900

    move actions

--- s4/T000006/TestCase.java
+++ s4/T000006/TestCase.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.Class;
 
 /*
 interface FrequencerInterface {     // This interface provides the design for frequency counter.
